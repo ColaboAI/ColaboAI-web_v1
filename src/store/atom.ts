ca18dd2cacdd0da6a2cca36b1695c4615dc49c3b
sgeunhi <alsrb582@snu.ac.kr>
2022-07-26 16:59:20 +0900

    Add recoil state

--- src/store/atom.ts
+++ src/store/atom.ts
@@ -1,6 +1,26 @@
 import { atom } from 'recoil';
 
-export const musicPlayState = atom<boolean>({
-  key: 'musicPlayState',
+export const audioState = atom<string>({
+  key: 'audioState',
+  default: '',
+});
+
+export const playState = atom<boolean>({
+  key: 'playState',
+  default: false,
+});
+
+export const playIdState = atom<any>({
+  key: 'playIdState',
+  default: '',
+});
+
+export const volumeState = atom<number>({
+  key: 'volumeState',
+  default: 50,
+});
+
+export const muteState = atom<boolean>({
+  key: 'muteState',
   default: false,
 });
