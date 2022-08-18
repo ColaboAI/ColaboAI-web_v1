945710842ed9367dcac83f93f2a334bb845d7af3
sgeunhi <alsrb582@snu.ac.kr>
2022-08-18 16:15:58 +0900

    Fix header.text

--- tests/header.test.tsx
+++ tests/header.test.tsx
@@ -3,28 +3,40 @@ import Header from '@src/components/header/header';
 import { RecoilRoot } from 'recoil';
 
 describe('Header', () => {
-  it('renders header', () => {
-    render(
+  const setup = () => {
+    const utils = render(
       <RecoilRoot>
         <Header />
       </RecoilRoot>,
     );
     const input = screen.getByPlaceholderText('검색하기');
+    const logo = screen.getByText('ColaboAI');
+    const loginButton = screen.getByText('로그인');
+    const registerButton = screen.getByText('회원가입');
+    return {
+      ...utils,
+      input,
+      logo,
+      loginButton,
+      registerButton,
+    };
+  };
+
+  it('renders header', () => {
+    const { input, logo, loginButton, registerButton } = setup();
+    expect(logo).toBeInTheDocument();
     expect(input).toBeInTheDocument();
+    expect(loginButton).toBeInTheDocument();
+    expect(registerButton).toBeInTheDocument();
   });
 
   it('changes input', () => {
-    render(
-      <RecoilRoot>
-        <Header />
-      </RecoilRoot>,
-    );
-    const input = screen.getByPlaceholderText('검색하기');
+    const { input } = setup();
     fireEvent.change(input, {
       target: {
-        value: 'COLABOAI_WEB',
+        value: '콜라보에이아이',
       },
     });
-    expect(input).toHaveAttribute('value', 'COLABOAI_WEB');
+    expect(input).toHaveAttribute('value', '콜라보에이아이');
   });
 });
