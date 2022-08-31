ed094314a45be4063886da1ca1d5657bb2e42e3d
GoGiants1 <chooi221@snu.ac.kr>
2022-08-31 14:46:57 +0900

    Add login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,11 +3,8 @@ import Layout from '@src/components/layout/layout';
 import type { NextPageWithLayout } from './_app';
 import { NextSeo } from 'next-seo';
 import styles from '/styles/register.module.scss';
-import { useRegister } from '@src/hooks/useRegister';
-// TODO: Register 컴포넌트에서 이메일 중복 체크를 하는 기능을 추가해야함.
 
 const Register: NextPageWithLayout = () => {
-  const [{ email, password }, onChangeForm, onClickRegister] = useRegister();
   return (
     <div>
       <NextSeo
@@ -27,25 +24,13 @@ const Register: NextPageWithLayout = () => {
         <p>음악의 새로운 패러다임</p>
         <div className={styles.input}>
           <div>
-            <input
-              name={'email'}
-              className={styles.info}
-              value={email}
-              onChange={(e) => onChangeForm(e)}
-              placeholder="아이디"
-            />
+            <input className={styles.info} placeholder="아이디" />
           </div>
           <div>
-            <input
-              name={'password'}
-              className={styles.info}
-              value={password}
-              onChange={(e) => onChangeForm(e)}
-              placeholder="패스워드"
-            />
+            <input className={styles.info} placeholder="패스워드" />
           </div>
           <div className={styles.login}>
-            <button onClick={() => onClickRegister()}>회원가입</button>
+            <button>로그인</button>
           </div>
           <div className={styles.social}>
             <button>구글</button>
